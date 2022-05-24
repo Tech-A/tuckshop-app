8a4af3801d6394fac060fe451ad773cef06b59ce
abbeym <78575085+Tech-A@users.noreply.github.com>
2022-05-24 22:46:35 +1200

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
-// In App.js in a new project
-
 import * as React from 'react';
 import { Image, StyleSheet, TouchableOpacity, TextInput, SafeAreaView, Button, View, Text } from 'react-native';
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import styles from './src/styles';
+
 import { useState, useEffect } from "react";
 import { db1 } from './src/firebase-config';
 import { collection, getDocs } from 'firebase/firestore';
-import MenuScreen from './src/components/MenuScreen';
 
+import styles from './src/styles';
+import MenuScreen from './src/screens/MenuScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import InfoScreen from './src/screens/InfoScreen';
 
 
 function HomeScreen({ navigation }) {
@@ -51,134 +52,6 @@ function HomeScreen({ navigation }) {
 }
 
 
-
-
-// function MenuScreen({ navigation }) {
-//   const [menu, setMenu] = useState([]);
-//   const menuCollectionRef = collection(db1, "menu");
-
-//   useEffect(() => {
-//     const getMenu = async () => {
-//       const data = await getDocs(menuCollectionRef);
-//       setMenu(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-//     };
-
-//     getMenu();
-//   }, []);
-// return (
-//   <View style={styles.App}> 
-//     {menu.map((menu) => { 
-//       return (
-    
-//       <View>
-//         <Text>Food: {menu.food}</Text>
-//         <Text>Cost: {menu.cost}</Text>
-//       </View>
-//       );
-//     })} 
-//     </View>
-//     );
-// }
-
-  
-  
-  
-
-
-// function ChoicesScreen({ navigation }) {
-//   return ( 
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text style={{ fontSize: 25, padding: 20, textAlign:'center' }}>How would you like to preorder?</Text>
-//       <TouchableOpacity
-//    style={styles.choicesbutton}
-//    onPress={() => navigation.navigate('Login')}>
-//  <Text style={styles.choicestext}>New/Returning customer</Text>
-//  <Text style={styles.choicessubtext}>Save your login details and preorder</Text>
-//    </TouchableOpacity>
-
-//    <TouchableOpacity
-//    style={styles.choicesbutton}
-//    onPress={() => navigation.navigate('Guest')}>
-//  <Text style={styles.choicestext}>One-time customer</Text>
-//  <Text style={styles.choicessubtext}>Preorder as guest</Text>
-//    </TouchableOpacity>
-// <Text></Text>
-
-//       <Button title="Back" onPress={() => navigation.navigate('Home')} />
-//     </View>
-//   );
-// }
-
-
-
-function InfoScreen({ navigation }) {
-  return ( 
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={styles.Infoheader}>Information/Updates about the Tuckshop!</Text>
-      
-      <Button title="Home" onPress={() => navigation.navigate('Home')} />
-    </View>
-  );
-}
-
-
-
-function LoginScreen({ navigation }) {  
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-    return (  
-      <><View style={styles.logincontainer}>
-        <Image source={{ uri: "https://photos.schoolpoint.co.nz/school/70/marist_logo.png" }}
-          style={{ width: 100, height: 100, marginBottom: 40, }} />
-
-        <StatusBar style="auto" />
-        <View style={styles.inputView}>
-          <TextInput
-            style={styles.TextInput}
-            placeholder="Email."
-            placeholderTextColor="#003f5c"
-            onChangeText={(email) => setEmail(email)} />
-        </View>
-
-        <View style={styles.inputView}>
-          <TextInput
-            style={styles.TextInput}
-            placeholder="Password."
-            placeholderTextColor="#003f5c"
-            secureTextEntry={true}
-            onChangeText={(password) => setPassword(password)} />
-        </View>
-
-        <TouchableOpacity>
-          <Text style={styles.forgot_button}>Forgot Password?</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
-
-
-      </View>
-      <View>
-          <TouchableOpacity>
-            <Text style={styles.adminbutton}>Admin Login</Text>
-          </TouchableOpacity>
-        </View></>
-    );
-  }
-  
-
-
-
-// function GuestScreen({ navigation }) {
-//   return ( 
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Fill out this form </Text>
-//       <Text></Text>
-//       <Button title="Home" onPress={() => navigation.navigate('Home')} />
-//     </View>
-//   );
-// }
 
 
 
