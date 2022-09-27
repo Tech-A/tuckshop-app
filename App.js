9b4706a87a52dfb748726cb1dc67347610190151
abbeym <78575085+Tech-A@users.noreply.github.com>
2022-09-27 15:53:25 +1300

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MenuScreen from './src/screens/MenuScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import InfoScreen from './src/screens/InfoScreen';
-import HomeScreen from './src/screens/HomeScreen';
-
-
+import MenuScreen from './src/screens/user/MenuScreen';
+import LoginScreen from './src/screens/user/LoginScreen';
+import InfoScreen from './src/screens/user/InfoScreen';
+import HomeScreen from './src/screens/user/HomeScreen';
+import HomeScreenA from './src/screens/admin/HomeScreenA';
+import MenuScreenA from './src/screens/admin/MenuScreenA';
+import InfoScreenA from './src/screens/admin/InfoScreenA';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,10 +17,11 @@ function App() {
       <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Menu" component={MenuScreen} />
-        {/* <Stack.Screen name="Preorder" component={ChoicesScreen} /> */}
         <Stack.Screen name="Info" component={InfoScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
-        {/* <Stack.Screen name="Guest" component={GuestScreen} /> */}
+        <Stack.Screen name="AdminHome" component={HomeScreenA} />
+        <Stack.Screen name="AdminMenu" component={MenuScreenA} />
+        <Stack.Screen name="AdminInfo" component={InfoScreenA} />
       </Stack.Navigator>
     </NavigationContainer>
   );
